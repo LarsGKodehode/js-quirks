99e49149c18ec361d54137cc5e2c4d2e8ea3a1b3
LarsGKodehode <104063134+LarsGKodehode@users.noreply.github.com>
2022-08-02 15:17:06 +0200

    Setup step 2 (#3)
    * liveServer minum implemented
    
    * finilized setup

---
+++ main.js
@@ -0,0 +1,15 @@
+import { MyClass } from "./encapsulation/encapuslation-class.js";
+
+const newClass = new MyClass;
+
+function log(thing) {
+  console.dir(thing);
+};
+
+log(newClass);
+
+log(newClass.publicMember);
+log(newClass.publicMethod());
+
+log(newClass.getAllMembers());
+log(newClass.getAllMethods());
