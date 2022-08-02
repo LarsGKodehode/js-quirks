96a0ed7b745e186ac1108b44ab24310e49d550bc
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-08-02 15:16:21 +0200

    finilized setup

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ import { MyClass } from "./encapsulation/encapuslation-class.js";
 
 const newClass = new MyClass;
 
-console.log("testing")
+function log(thing) {
+  console.dir(thing);
+};
+
+log(newClass);
+
+log(newClass.publicMember);
+log(newClass.publicMethod());
+
+log(newClass.getAllMembers());
+log(newClass.getAllMethods());
